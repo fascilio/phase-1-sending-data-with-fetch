4221c51412a795b23c1d96464aecf7ef2d104065
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-03-27 23:14:15 +0300

    sending POST

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+    const data = {name: name, email: email};
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(json => {
+        const newId = json.id;
+        document.body.innerHTML += newId;
+      })
+      .catch(error => {
+        document.body.innerHTML += error.message;
+      });
+  }
+  console.log(submitData("John Doe", "johndoe@example.com"));
+  
+
